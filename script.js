6f83c6db7462237d414f10e43fe313cf86803534
Ayaan <ayaanot07@gmail.com>
2025-07-30 20:32:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-const Globe = window.Globe;
-const myGlobe = Globe()
-  (document.getElementById('globe-container'))
-  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
-  .backgroundColor('#000')
-  .showAtmosphere(true)
-  .atmosphereColor('#3a228a')
-  .atmosphereAltitude(0.25);
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #000;
+  color: white;
+  text-align: center;
+}
 
-// 👉 Dynamically load stadium data
-fetch('https://raw.githubusercontent.com/mapsam/world-stadiums/master/data/stadiums.json')
-  .then(res => res.json())
-  .then(data => {
-    const points = data.map(s => ({
-      lat: parseFloat(s.latitude),
-      lng: parseFloat(s.longitude),
-      name: s.name
-    }));
+h1 {
+  margin-top: 20px;
+}
 
-    myGlobe
-      .pointsData(points)
-      .pointLat(d => d.lat)
-      .pointLng(d => d.lng)
-      .pointAltitude(0.03)
-      .pointColor(() => 'orange')
-      .pointLabel(d => d.name);
-  })
-  .catch(err => console.error('Error loading stadium data', err));
+#globe-container {
+  width: 100vw;
+  height: 90vh;
+  overflow: hidden;
+}
