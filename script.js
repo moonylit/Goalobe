a7f7bb9eed25a3272317d8db5dde76b1c70a88b1
Ayaan <ayaanot07@gmail.com>
2025-07-30 20:15:48 +0530

    Update script.js
    Dynamic stadium locations added

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ const myGlobe = Globe()
   .showAtmosphere(true)
   .atmosphereColor('#3a228a')
   .atmosphereAltitude(0.25);
+
+// 👉 Dynamically load stadium data
+fetch('https://raw.githubusercontent.com/mapsam/world-stadiums/master/data/stadiums.json')
+  .then(res => res.json())
+  .then(data => {
+    const points = data.map(s => ({
+      lat: parseFloat(s.latitude),
+      lng: parseFloat(s.longitude),
+      name: s.name
+    }));
+
+    myGlobe
+      .pointsData(points)
+      .pointLat(d => d.lat)
+      .pointLng(d => d.lng)
+      .pointAltitude(0.03)
+      .pointColor(() => 'orange')
+      .pointLabel(d => d.name);
+  })
+  .catch(err => console.error('Error loading stadium data', err));
